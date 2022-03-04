7d15e43fa787388cf741a89fd8739f1b19973df1
Celina Takeda <takedacarina@gmail.com>
2022-03-04 11:04:36 -0300

    commit

---
+++ asyn-await/0.promise.js
@@ -0,0 +1,51 @@
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('End'), 5000)
+})
+/*
+// IIFE
+(async function() {
+    console.log('Olá mundo')
+})()
+// Or
+// async faz a mesma coisa que a promise
+(function () {
+    return new Promise((resolve, reject) => {
+        console.log('Olá mundo')
+        resolve()
+    })
+})()
+*/
+/*
+console.log('Begin')
+promise.then((text) => console.log(text))
+// Or
+promise.then(console.log)
+// Or
+promise.then(
+    (res) => setTimeout(() => console.log(res), 5000),
+    (rej) => console.error(rej)
+)
+*/
+
+// usando async-await
+(async function () {
+    console.log('Begin')
+
+    const text = await promise
+    console.log(text)
+
+    // Or
+    console.log(await promise)
+
+    try {
+        const res = await promise
+        setTimeout(() => console.log(res), 5000)
+    }catch (rej) {
+        console.error(rej)
+    }
+    // Or
+    promise.then(
+        (res) => setTimeout(() => console.log(res), 5000),
+        (rej) => console.error(rej)
+)
+})()
